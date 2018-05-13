65bb6ab1e0513087bd64d66600d05ee7d776f786
zzzhang <1286179845@qq.com>
2018-05-13 22:34:28 +0800

    Revert "Revert "emmm""
    This reverts commit f395f9a2022c106e51aa24bc4a35872ddd37deea.

--- pages/register2/register.js
+++ pages/register2/register.js
@@ -18,7 +18,7 @@ function timeDown (that) {
   var countNumber = that.data.countNumber;
   if (countNumber == 0) {
     that.setData({
-      countNumber: 300,
+      countNumber: 60,
       getCode: '重新获取'
     })
     return;
@@ -49,7 +49,7 @@ Page({
     eyeImage: '/images/eye-close.png',
     isPassword: true,
     getCode:'获取验证码',
-    countNumber:300,
+    countNumber:60,
   },
 
   //事件处理函数
@@ -150,10 +150,10 @@ Page({
       })
       return;
     }
-    if (this.data.countNumber != 0 && this.data.countNumber != 300) {
+    if (this.data.countNumber != 0 && this.data.countNumber != 60) {
       return
     }
-    if (this.data.countNumber==300){
+    if (this.data.countNumber==60){
       timeDown(this);
     }
     var MobileNo = this.data.userPhonenumber
